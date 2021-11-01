4f2f767567f5c247c8c7abda286bddedb113b762
EnricoFiorini97 <enricofiorini899@gmail.com>
2021-11-01 18:02:43 +0100

    updating csv dataframe

--- POEC/poec.py
+++ POEC/poec.py
@@ -14,22 +14,10 @@ import statsmodels.api as sm
 # Machine Learning
 from sklearn import model_selection, preprocessing, feature_selection, ensemble, linear_model, metrics, decomposition
 
-import spacy
 
 def main() -> None:
-    df = pd.read_csv("../raw_dataset/threads-g2-new-ng.txt", delimiter='\t')
-    nlp = spacy.load("en_core_web_sm")
-
-    report = ""
-    for _, line in enumerate(df["FULL_TEXT"]):
-        line2 = nlp(line)
-        for token in line2.ents:
-            if token.label_ == 'PERSON':
-                report += (line + '\n')
-                break
-
-    with open ("tmp.txt", "w") as f:
-        f.write(report)
+    df = pd.read_csv("dataset.csv", delimiter=',')
+    print(df)
 
 if __name__ == "__main__":
     main()
